43db3991becf5879b26daf9859c4e84f63aec2e6
Jose Medina <fermed28@gmail.com>
2019-04-30 21:30:12 +0000

    fixes style code

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
-for number in range(0,99):
-    print("{:d}{:d}".format(int(number/10), int(number%10)), end=", ")
+for number in range(0, 99):
+    print("{:d}{:d}".format(int(number / 10), int(number % 10)), end=", ")
 print(99)
