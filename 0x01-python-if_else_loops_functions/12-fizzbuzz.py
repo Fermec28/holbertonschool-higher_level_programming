1bc3647cb74879a3371a48bd1dd0a31f090cb38c
Jose Medina <fermed28@gmail.com>
2019-05-01 04:08:06 +0000

    fizz buzz solution

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -1,3 +1,11 @@
 #!/usr/bin/python3
-for number in range(1:101):
-    print number
+def fizzbuzz():
+    for number in range(1,101):
+        if number % 3 == 0 and number % 5 == 0:
+            print("FizzBuzz", end=" ")
+        elif number % 3 == 0:
+            print("Fizz", end=" ")
+        elif number % 5 == 0:
+            print("Buzz", end=" ")
+        else:
+            print("{:d}".format(number), end=" ")
