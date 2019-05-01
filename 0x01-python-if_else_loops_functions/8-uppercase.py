a86463929f451db850a37aadc1555c15200e9be2
Jose Medina <fermed28@gmail.com>
2019-05-01 00:05:50 +0000

    To upper

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,1 +1,13 @@
 #!/usr/bin/python3
+def islower(c):
+    if ord(c) >= 97 and ord(c) <= 122:
+        return True
+    else:
+        return False
+
+def uppercase(str):
+    for letter in str:
+        if islower(letter):
+            letter = "{:c}".format(ord(letter) - 97 + 65)
+        print(letter, end="")
+    print("")
