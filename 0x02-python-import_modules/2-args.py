11f0ed0bce070db93986c830981ffe111912406f
Jose Medina <fermed28@gmail.com>
2019-05-03 17:01:51 +0000

    print arguments

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,1 +1,10 @@
 #!/usr/bin/python3
+if __name__ == "__main__":
+    import sys
+    if len(sys.argv) == 1:
+        print("{:d} arguments.".format(len(sys.argv) - 1))
+    else:
+        pluralize = "s" if len(sys.argv) > 2 else ""
+        print("{:d} argument{:s}:".format(len(sys.argv) - 1, pluralize))
+    for index in range(1, len(sys.argv)):
+        print("{:d}: {:s}".format(index, sys.argv[index]))
