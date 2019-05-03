6ead462ed42b39f8769e4bc9f7b7dea2dd67b6f3
Jose Medina <fermed28@gmail.com>
2019-05-03 17:25:55 +0000

    infinite sum

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,1 +1,8 @@
 #!/usr/bin/python3
+if __name__ == "__main__":
+    import sys
+    sum = 0
+    if len(sys.argv) > 1:
+        for index in range(1, len(sys.argv)):
+            sum = sum + int(sys.argv[index])
+    print("{:d}".format(sum))
