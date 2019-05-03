7bfc87d9cb7283348bcf8708a882476ac66f31d3
Jose Medina <fermed28@gmail.com>
2019-05-03 18:37:56 +0000

    print functions

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,1 +1,6 @@
 #!/usr/bin/python3
+if __name__ == "__main__":
+    import hidden_4
+    for function in dir(hidden_4):
+        if function[0] != '_' and function[1] != '_':
+            print(function)
