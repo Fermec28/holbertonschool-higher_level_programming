b67b4e553c1f84f77ee87ea42d58f2f3be55d984
Jose Medina <fermed28@gmail.com>
2019-05-07 19:08:58 +0000

    change boundary

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def new_in_list(my_list, idx, element):
     copy = my_list[:]
-    if idx < len(copy) and idx > 0:
+    if idx < len(copy) and idx >= 0:
         copy[idx] = element
         return copy
     else:
