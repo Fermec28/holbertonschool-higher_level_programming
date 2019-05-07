bf4cff1504c8c7ace65e707de6759533a1f55b0c
Jose Medina <fermed28@gmail.com>
2019-05-07 21:26:41 +0000

    add cases

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -1,2 +1,9 @@
 #!/usr/bin/python3
 def add_tuple(tuple_a=(), tuple_b=()):
+    arr = [0, 0]
+    for x in range(2):
+        if tuple_a[x]:
+            arr[x] += tuple_a[x]
+        if tuple_b[x]:
+            arr[x] += tuple_b[x]
+    return (arr[0], arr[1])
