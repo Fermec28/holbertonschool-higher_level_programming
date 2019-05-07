98d7c9304dbdbeee38213486c7062ce3994ac26d
Jose Medina <fermed28@gmail.com>
2019-05-07 19:18:11 +0000

    changes

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -2,6 +2,5 @@
 def print_reversed_list_integer(my_list=[]):
     aux = my_list[:]
     aux.reverse()
-    if aux:
-        for i in aux:
-            print("{:d}".format(i))
+    for i in aux:
+        print("{:d}".format(i))
