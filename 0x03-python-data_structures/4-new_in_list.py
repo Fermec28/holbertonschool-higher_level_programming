9e3455959be68024d6b872bec9b55cdb92eb1da9
Jose Medina <fermed28@gmail.com>
2019-05-07 18:56:01 +0000

    fixex pep8

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def new_in_list(my_list, idx, element):
-    copy =  my_list[:]
+    copy = my_list[:]
     if idx < len(copy) and idx > 0:
         copy[idx] = element
     return copy
