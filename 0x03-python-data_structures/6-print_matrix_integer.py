7ac0256a5ea90049ea5b64a04107cce632cdf62a
Jose Medina <fermed28@gmail.com>
2019-05-07 20:07:01 +0000

    ugly but functional

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,2 +1,11 @@
-#!/usr/bin/python3
+#!/bin/usr/python3
 def print_matrix_integer(matrix=[[]]):
+    for row in matrix:
+        if len(row) > 0:
+            for i in range(len(row)):
+                if i == len(row) - 1:
+                    print("{:d}".format(row[i]))
+                else:
+                    print("{:d} ".format(row[i]), end="")
+        else:
+            print()
