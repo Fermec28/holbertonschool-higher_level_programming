2898121ca10ba1ecc36aba16d45c918138c7afdd
Jose Medina <fermed28@gmail.com>
2019-05-09 22:41:59 +0000

    calculate best score

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,2 +1,13 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
+    if a_dictionary != None:
+        aux = {}
+        aux["key"] = list(a_dictionary.keys())[0]
+        aux["val"] = a_dictionary[aux["key"]]
+        for key in a_dictionary.keys():
+            if a_dictionary[key] > aux["val"]:
+                aux["key"] = key
+                aux["val"] = a_dictionary[key]
+        return aux["key"]
+    else:
+        return None
