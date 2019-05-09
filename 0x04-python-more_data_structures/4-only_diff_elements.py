8831c00ec4a17596961fcde3954a4818645aae3f
Jose Medina <fermed28@gmail.com>
2019-05-09 21:00:56 +0000

    Fixes Pep8

--- 0x04-python-more_data_structures/4-only_diff_elements.py
+++ 0x04-python-more_data_structures/4-only_diff_elements.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def only_diff_elements(set_1, set_2):
     aux = []
-    aux = list(filter(lambda x: not x in set_1,set_2))
-    aux.extend(list(filter(lambda x: not x in set_2,set_1)))
+    aux = list(filter(lambda x:  x not in set_1, set_2))
+    aux.extend(list(filter(lambda x: x not in set_2, set_1)))
     return aux
