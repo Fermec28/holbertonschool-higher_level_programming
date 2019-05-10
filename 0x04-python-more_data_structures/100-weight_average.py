6d5bbe8eded4e1063ff9296e474e88d6d5c2a2fa
Jose Medina <fermed28@gmail.com>
2019-05-10 00:59:11 +0000

    average

---
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+def weight_average(my_list=[]):
+    if my_list:
+        return multiply_elements(my_list) / sum_elements(my_list)
+    else:
+        return None
+
+def multiply_elements(my_list=[]):
+    aux = 0
+    for x in my_list:
+        aux += x[0] * x[1]
+    return aux
+
+def sum_elements(my_list=[]):
+    aux = 0
+    for x in my_list:
+        aux += x[1]
+    return aux
--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,1 +1,11 @@
 #!/usr/bin/python3
+def roman_to_int(roman_string):
+    letters = {
+        "I": 1,
+        "V": 5,
+        "X": 10,
+        "L": 50,
+        "C": 100,
+        "D": 500,
+        "M": 1000
+    }
