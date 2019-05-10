d946e3c85bac52692b399a52d62aacce8dea7c1f
Jose Medina <fermed28@gmail.com>
2019-05-10 01:00:57 +0000

    return 0 fixing pep8

--- 0x04-python-more_data_structures/100-weight_average.py
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -3,13 +3,15 @@ def weight_average(my_list=[]):
     if my_list:
         return multiply_elements(my_list) / sum_elements(my_list)
     else:
-        return None
+        return 0
+
 
 def multiply_elements(my_list=[]):
     aux = 0
     for x in my_list:
         aux += x[0] * x[1]
     return aux
+
 
 def sum_elements(my_list=[]):
     aux = 0
