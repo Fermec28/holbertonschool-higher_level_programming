037d1bb2de2d3588f9c416fafc9239c098e8aaba
Jose Medina <fermed28@gmail.com>
2019-05-21 02:33:24 +0000

    adding :

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -2,8 +2,9 @@
 def safe_print_division(a, b):
     try:
         b = "{:.1f}".format(a / b)
+        return b
     except:
         b = "None"
+        return None
     finally:
-        print("Inside result {}".format(b))
-        return b
+        print("Inside result: {}".format(b))
