3a6b77ac02ebee4e4cb7d2a0f59c679d3839649c
Jose Medina <fermed28@gmail.com>
2019-05-21 02:29:34 +0000

    use finally

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -1,2 +1,9 @@
 #!/usr/bin/python3
 def safe_print_division(a, b):
+    try:
+        b = "{:.1f}".format(a / b)
+    except:
+        b = "None"
+    finally:
+        print("Inside result {}".format(b))
+        return b
