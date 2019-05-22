36d6f8274a756fe437f8322e46b0dcdee62b5f11
Jose Medina <fermed28@gmail.com>
2019-05-22 01:54:14 +0000

    add case when size is zero

--- 0x06-python-classes/5-square.py
+++ 0x06-python-classes/5-square.py
@@ -19,5 +19,8 @@ class Square:
         return self.__size ** 2
 
     def my_print(self):
-        for i in range(self.size):
-            print("#" * self.size)
+        if self.size == 0:
+            print()
+        else:
+            for i in range(self.size):
+                print("#" * self.size)
