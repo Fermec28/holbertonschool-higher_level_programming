abc0267f8a03e2ddd98accd93c5b3b0453e6dbb9
Jose Medina <fermed28@gmail.com>
2019-05-22 12:58:54 +0000

    sorting linked list

---
+++ 0x06-python-classes/100-singly_linked_list.py
@@ -0,0 +1,59 @@
+class Node:
+    def __init__(self, data, next_node=None):
+        self.data = data
+        self.next_node = next_node
+
+    @property
+    def data(self):
+        return self.__data
+
+    @property
+    def next_node(self):
+        return self.__next_node
+
+    @data.setter
+    def data(self, value):
+        if type(value) is not int:
+            print("errror")
+            raise TypeError("data must be an integer")
+        else:
+            self.__data = value
+
+    @next_node.setter
+    def next_node(self, value):
+        if value is not None and type(value) is not Node:
+            raise TypeError("next_node must be a Node object")
+        self.__next_node = value
+
+class SinglyLinkedList:
+    def __init__(self):
+        self.__head = None
+
+    def sorted_insert(self, value):
+        if self.__head == None:
+           self.__head = Node(value)
+        else:
+            aux = self.__head
+            if  value <= aux.data:
+                self.__head = Node(value, aux)
+            else:
+                while(aux.next_node != None):
+                    if (aux.next_node.data >= value):
+                        new_node = Node(value, aux.next_node)
+                        aux.next_node = new_node
+                        break
+                    else:
+                        aux = aux.next_node
+                if aux.next_node == None:
+                    aux.next_node = Node(value)
+
+    def __str__(self):
+        aux = self.__head
+        str = ""
+        while(aux != None):
+            if (aux.next_node is not None):
+                str = "{}{:d}\n".format(str, aux.data)
+            else:
+                str = "{}{:d}".format(str, aux.data)
+            aux = aux.next_node
+        return str
