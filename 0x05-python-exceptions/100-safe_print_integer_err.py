92ca1988981fedd01fef0fb744cc85275db6a6c3
Jose Medina <fermed28@gmail.com>
2019-05-23 21:01:03 +0000

    add solution prevent int

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -1,2 +1,11 @@
 #!/usr/bin/python3
+import sys
+
+
 def safe_print_integer_err(value):
+    try:
+        print("{:d}".format(value))
+        return True
+    except (TypeError, ValueError) as msg:
+        print("Exception: {}".format(msg), file=sys.stderr)
+        return False
