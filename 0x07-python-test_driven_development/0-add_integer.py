92a06d70b8ade044c662c1a20da9f54259d07b65
Jose Medina <fermed28@gmail.com>
2019-05-24 23:10:59 +0000

    adding comments correctly with pep8

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,5 +1,17 @@
 #!/usr/bin/python3
+"""
+This module contains a function to add two numbers
+"""
+
+
 def add_integer(a, b=98):
+    """
+    Add two numbers
+    Parameters:
+    @a: Number1
+    @b: Number2
+    Return: Sum of two numbers
+    """
     if type(a) is not int and type(a) is not float:
         raise TypeError("a must be an integer")
     if type(b) is not int and type(b) is not float:
