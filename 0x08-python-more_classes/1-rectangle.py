9a7a93cae8052f995685a0394f4b1c927ffd63b2
Jose Medina <fermed28@gmail.com>
2019-05-27 20:21:29 +0000

    simple rectangle

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -1,1 +1,24 @@
 #!/usr/bin/python3
+class Rectangle:
+    def __init__(self, width=0, height=0):
+        self.width = width
+        self.height = height
+
+    @property
+    def width(self):
+        return self.width
+
+
+    @property
+    def height(self):
+        return self.height
+
+
+    @width.setter
+    def width(self, value):
+        self.__width = value
+
+
+    @height.setter
+    def height(self, value):
+        self.__height = value
