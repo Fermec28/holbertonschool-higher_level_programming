3ec166cc7dc12243a5a62e3bb3c405bdb74d33d3
Jose Medina <fermed28@gmail.com>
2019-07-15 21:51:24 +0000

    [fixes] as number

--- 0x0D-SQL_introduction/15-groups.sql
+++ 0x0D-SQL_introduction/15-groups.sql
@@ -1,5 +1,5 @@
 -- script that lists the number of records with the same score in the table
-SELECT score, COUNT(score) AS average
+SELECT score, COUNT(score) AS number
 FROM second_table
 GROUP BY score
 ORDER BY COUNT(score) DESC;
