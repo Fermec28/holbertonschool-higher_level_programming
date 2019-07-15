43f10ffa73fb89378f0c734421c20a1bdb531487
Jose Medina <fermed28@gmail.com>
2019-07-15 17:04:22 +0000

    [add]  preparing project

---
+++ 0x0D-SQL_introduction/1-create_database_if_missing.sql
@@ -0,0 +1,3 @@
+/*script that create the database hbtn_0c_0 in your MySQL server*/
+/*Create Database*/
+CREATE DATABASE hbtn_0c_0;
---
+++ 0x0D-SQL_introduction/2-remove_database.sql
@@ -0,0 +1,3 @@
+/*script that deletes the database hbtn_0c_0 in your MySQL server*/
+/*Delete Database*/
+DROP DATABASE hbtn_0c_0;
