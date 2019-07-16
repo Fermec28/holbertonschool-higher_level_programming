c68bda6935572372471f64985b9e5f7f4792de61
Jose Medina <fermed28@gmail.com>
2019-07-16 20:59:17 +0000

    add UNIQUE constrain

--- 0x0E-SQL_more_queries/7-cities.sql
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -1,7 +1,7 @@
 -- script that creates the database hbtn_0d_usa and the table states
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
 CREATE TABLE IF NOT EXISTS hbtn_0d_usa.cities(
-       id INT NOT NULL AUTO_INCREMENT,
+       id INT NOT NULL AUTO_INCREMENT UNIQUE,
        state_id INT NOT NULL,
        name VARCHAR(256),
        PRIMARY KEY (id),
