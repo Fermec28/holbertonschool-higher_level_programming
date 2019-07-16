b66f03361f09911f95d27db476ac02bf1897226a
Jose Medina <fermed28@gmail.com>
2019-07-16 22:15:02 +0000

    [10] inner join

---
+++ 0x0E-SQL_more_queries/10-genre_id_by_show.sql
@@ -0,0 +1,7 @@
+-- that lists all shows contained in hbtn_0d_tvshows that have at least one genre linked.
+SELECT tv_shows.title ,tv_show_genres.genre_id
+FROM tv_shows
+INNER JOIN tv_show_genres
+ON tv_show_genres.show_id = tv_shows.id
+WHERE tv_show_genres.genre_id IS NOT NULL
+ORDER BY tv_shows.title, tv_show_genres.genre_id;
