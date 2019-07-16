d83a38c7d0b2b3996c6eda75b442ceb7b25c3a30
Jose Medina <fermed28@gmail.com>
2019-07-16 03:55:13 +0000

    [102] top 3

---
+++ 0x0D-SQL_introduction/102-top_city.sql
@@ -0,0 +1,7 @@
+-- script that displays the average temperature (Fahrenheit)
+SELECT city, AVG(value) AS avg_temp
+FROM temperatures
+WHERE month = 7 OR month = 8
+GROUP BY city
+ORDER BY avg_temp DESC
+LIMIT 3
