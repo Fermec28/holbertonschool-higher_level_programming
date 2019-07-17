c5b02efc8813915ba7b001f4720c26948a560239
Jose Medina <fermed28@gmail.com>
2019-07-17 03:08:05 +0000

    add .name to order

--- 0x0E-SQL_more_queries/16-shows_by_genre.sql
+++ 0x0E-SQL_more_queries/16-shows_by_genre.sql
@@ -5,4 +5,4 @@ LEFT JOIN tv_show_genres
  ON  tv_shows.id = tv_show_genres.show_id
 LEFT JOIN tv_genres
  ON  tv_show_genres.genre_id = tv_genres.id
-ORDER BY tv_shows.title;
+ORDER BY tv_shows.title, tv_genres.name;
