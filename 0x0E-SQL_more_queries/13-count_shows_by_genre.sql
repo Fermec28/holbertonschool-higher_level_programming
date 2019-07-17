167f461dd01991add11183bc5d5984c123965496
Jose Medina <fermed28@gmail.com>
2019-07-17 06:34:11 +0000

    prueba sobra where

--- 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -3,5 +3,4 @@ SELECT tv_genres.name AS genre, COUNT(*) AS number_of_shows
 FROM tv_genres
 INNER JOIN  tv_show_genres
 ON tv_genres.id = tv_show_genres.genre_id
-WHERE tv_show_genres.show_id  IS NOT NULL
 GROUP BY tv_genres.name ORDER BY number_of_shows DESC
