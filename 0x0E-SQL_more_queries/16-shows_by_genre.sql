def3f5222e6b36ba2ff41b453d8f3cabea40883c
Jose Medina <fermed28@gmail.com>
2019-07-17 01:52:13 +0000

    [16] shows by genre

---
+++ 0x0E-SQL_more_queries/16-shows_by_genre.sql
@@ -0,0 +1,8 @@
+-- Write a script that lists all Comedy shows in the database
+SELECT tv_shows.title, tv_genres.name
+FROM tv_shows
+LEFT JOIN tv_show_genres
+ON  tv_shows.id = tv_show_genres.genre_id
+LEFT JOIN tv_genres
+ON  tv_show_genres.genre_id = tv_genres.id
+ORDER BY tv_shows.title;
