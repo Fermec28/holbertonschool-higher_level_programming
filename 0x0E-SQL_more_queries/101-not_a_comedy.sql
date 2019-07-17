492f7099150b04473fecb10b576d4348f7338b26
Jose Medina <fermed28@gmail.com>
2019-07-17 04:26:58 +0000

    same as previus

---
+++ 0x0E-SQL_more_queries/101-not_a_comedy.sql
@@ -0,0 +1,13 @@
+-- Write a script that lists all shows without the genre Comedy in the database 
+SELECT tv_shows.title
+FROM tv_shows
+WHERE tv_shows.id NOT IN (
+      SELECT tv_shows.id
+      FROM  tv_shows
+      INNER JOIN tv_show_genres
+      ON  tv_shows.id = tv_show_genres.show_id
+      INNER JOIN tv_genres
+       ON tv_genres.id = tv_show_genres.genre_id
+       WHERE tv_genres.name = 'Comedy'
+)
+ORDER BY title;
