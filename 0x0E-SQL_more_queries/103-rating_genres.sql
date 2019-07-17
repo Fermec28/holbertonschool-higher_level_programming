03c75d79672746c6de0169ecd3bef89afe0c635d
Jose Medina <fermed28@gmail.com>
2019-07-17 04:54:33 +0000

    same with more tables

---
+++ 0x0E-SQL_more_queries/103-rating_genres.sql
@@ -0,0 +1,11 @@
+--  lists all shows from hbtn_0d_tvshows_rate by their rating.
+SELECT tv_genres.name, SUM(tv_show_ratings.rate) AS rating
+FROM tv_genres
+INNER JOIN tv_show_genres
+ON tv_show_genres.genre_id = tv_genres.id
+INNER JOIN tv_shows
+ON tv_show_genres.show_id = tv_shows.id
+INNER JOIN tv_show_ratings
+ON tv_shows.id = tv_show_ratings.show_id
+GROUP BY tv_genres.name
+ORDER BY rating DESC;
