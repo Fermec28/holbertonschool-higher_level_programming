c2b6a7407cb727e027b189f80f7fc18ea1cd6c61
Jose Medina <fermed28@gmail.com>
2019-07-17 00:03:57 +0000

    fixes correctly the show name

--- 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -1,5 +1,5 @@
 -- lists all genres from
-SELECT tv_genres.name, COUNT(*) AS number_of_shows
+SELECT tv_genres.name AS genre, COUNT(*) AS number_of_shows
 FROM tv_genres
 INNER JOIN  tv_show_genres
 ON tv_genres.id = tv_show_genres.genre_id
