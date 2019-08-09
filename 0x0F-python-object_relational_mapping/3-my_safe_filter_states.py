422dd91f2d7cb64660553da4cd8c751c0f8ae213
Jose Medina <fermed28@gmail.com>
2019-08-09 22:56:28 +0000

    [add] join query

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -1,1 +1,17 @@
 #!/usr/bin/python3
+import sys
+import MySQLdb
+
+if __name__ == "__main__":
+    if(len(sys.argv) == 5):
+        conn = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1],
+                               passwd=sys.argv[2], db=sys.argv[3],
+                               charset="utf8")
+        cur = conn.cursor()
+        cur.execute("SELECT * FROM states WHERE NAME='{}'\
+        ORDER BY id ASC".format(sys.argv[4].split(";")[0].split('\'')[0]))
+        query_rows = cur.fetchall()
+        for row in query_rows:
+            print(row)
+        cur.close()
+        conn.close()
--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -1,1 +1,19 @@
 #!/usr/bin/python3
+import sys
+import MySQLdb
+
+if __name__ == "__main__":
+    if(len(sys.argv) == 4):
+        conn = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1],
+                               passwd=sys.argv[2], db=sys.argv[3],
+                               charset="utf8")
+        cur = conn.cursor()
+        cur.execute("SELECT cities.id, cities.name, states.name FROM states \
+        INNER JOIN cities \
+        ON cities.state_id  = states.id\
+        ORDER BY cities.id ASC")
+        query_rows = cur.fetchall()
+        for row in query_rows:
+            print(row)
+        cur.close()
+        conn.close()
