06dd33691946748b5b7b8eb9c9b84ebd61dcda7c
Jose Medina <fermed28@gmail.com>
2019-08-10 19:00:40 +0000

    [add] adding documentation

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
+""" takes in an argument and displays all values in the states """
 import sys
 import MySQLdb
 
@@ -8,7 +9,7 @@ if __name__ == "__main__":
                                passwd=sys.argv[2], db=sys.argv[3],
                                charset="utf8")
         cur = conn.cursor()
-        cur.execute("SELECT * FROM states WHERE NAME='{}'\
+        cur.execute("SELECT id, name FROM states WHERE NAME='{}'\
         ORDER BY id ASC".format(sys.argv[4]))
         query_rows = cur.fetchall()
         for row in query_rows:
