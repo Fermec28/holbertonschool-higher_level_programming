78b64a333cec1364fafd3f589ed57d299996f9a7
Ferney Medina <fermed28@gmail.com>
2019-08-12 04:25:27 +0000

    [add] delete record

--- 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
+++ 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
@@ -1,1 +1,21 @@
 #!/usr/bin/python3
+""" lists all State objects that contain the letter a """
+
+
+from sqlalchemy import (create_engine)
+from sqlalchemy.orm import sessionmaker
+from model_state import Base, State
+from sys import argv
+
+
+if __name__ == '__main__':
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(argv[1],
+                           argv[2], argv[3]),
+                           pool_pre_ping=True)
+    Base.metadata.create_all(engine)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    states = session.query(State).filter(State.name.like("%a%"))
+    states = states.delete(synchronize_session=False)
+    session.commit()
+    session.close()
