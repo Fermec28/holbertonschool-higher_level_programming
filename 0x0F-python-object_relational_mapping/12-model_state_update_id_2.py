fd1041561f5dac05b965f79ba38687e64eeb36aa
Ferney Medina <fermed28@gmail.com>
2019-08-12 04:14:01 +0000

    update record attribute

--- 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
+++ 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
@@ -1,1 +1,22 @@
 #!/usr/bin/python3
+""" lists all State objects that contain the letter a """
+
+
+from sqlalchemy import (create_engine)
+from sqlalchemy.orm import sessionmaker
+from model_state import Base, State
+from sys import argv
+
+
+if __name__ == '__main__':
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(argv[1],
+                           argv[2], argv[3]),
+                           pool_pre_ping=True)
+    Base.metadata.create_all(engine)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    state = session.query(State).get(2)
+    if state:
+        state.name = 'New Mexico'
+        session.commit()
+    session.close()
