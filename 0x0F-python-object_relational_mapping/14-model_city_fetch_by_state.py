a77e00148bba1394ec4b8e549528cf250f9c8b34
Ferney Medina <fermed28@gmail.com>
2019-08-12 07:02:34 +0000

    [add] adding join association

---
+++ 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+""" lists all State objects from the database"""
+
+
+from sqlalchemy import (create_engine)
+from sqlalchemy.orm import sessionmaker
+from model_state import Base, State
+from model_city import City
+from sys import argv
+
+
+if __name__ == '__main__':
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(argv[1],
+                           argv[2], argv[3]),
+                           pool_pre_ping=True)
+    Base.metadata.create_all(engine)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    cities = session.query(State,City).join(State).order_by(State.id).all()
+    for state, city in cities:
+        print("{}: ({}) {}".format(state.name, city.id, city.name))
+    session.close()
