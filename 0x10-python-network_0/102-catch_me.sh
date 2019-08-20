e391d83a27f5e738f5d6332a6a5048678296d017
Jose Medina <fermed28@gmail.com>
2019-08-20 23:24:36 +0000

    add Origin

--- 0x10-python-network_0/102-catch_me.sh
+++ 0x10-python-network_0/102-catch_me.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Send Header
-curl -s "$1" -sLH "X-HolbertonSchool-User-Id:98" -X POST -d "name=You got me!"
+curl -s -L -H"X-HolbertonSchool-User-Id:98" -H "Origin: HolbertonSchool" -X PUT -d "user_id=98" 0.0.0.0:5000/catch_me
