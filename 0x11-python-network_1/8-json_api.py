b47f172ce8bc6b83c9c83b8b1c732c9737205691
Jose Medina <fermed28@gmail.com>
2019-08-26 23:00:48 +0000

    [add] handling json response

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -1,1 +1,18 @@
 #!/usr/bin/python3
+""" request to api  """
+
+if __name__ == "__main__":
+    import requests
+    from sys import argv
+    data = {'q': ""}
+    if len(argv) > 1:
+        data['q'] = argv[1]
+    response = requests.post(url="http://0.0.0.0:5000/search_user", data=data)
+    try:
+        data_response = eval(response.text)
+        if data_response:
+            print("[{}] {}".format(data_response['id'], data_response['name']))
+        else:
+            print("No result")
+    except Exception as err:
+        print("Not a valid JSON")
