86ad477a4992ec4e949be855e7b89551387664d1
Jose Medina <fermed28@gmail.com>
2019-08-26 21:18:05 +0000

    [add] handling error on requets

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -1,1 +1,11 @@
 #!/usr/bin/python3
+""" catching errors """
+if __name__ == "__main__":
+    import urllib.request
+    from sys import argv
+    try:
+        with urllib.request.urlopen(argv[1]) as response:
+            html = response.read()
+            print(html.decode('utf-8'))
+    except urllib.error.HTTPError as e:
+        print("Error code: {}".format(e.code))
