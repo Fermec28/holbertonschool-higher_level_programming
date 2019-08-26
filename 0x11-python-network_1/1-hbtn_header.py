633a61dcf6713e70aae3327e600606f5a4ba0011
Jose Medina <fermed28@gmail.com>
2019-08-26 20:34:48 +0000

    [add] get header form request with urllib

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -1,1 +1,7 @@
 #!/usr/bin/python3
+""" Take header from request"""
+if __name__ == "__main__":
+    import urllib.request
+    from sys import argv
+    with urllib.request.urlopen(argv[1]) as response:
+        print(response.getheader('X-Request-Id'))
