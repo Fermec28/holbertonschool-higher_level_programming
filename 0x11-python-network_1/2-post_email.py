3c5b08179f622a62a354f56244a20770e2974292
Jose Medina <fermed28@gmail.com>
2019-08-26 21:01:13 +0000

    [add] post request

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -1,1 +1,16 @@
 #!/usr/bin/python3
+""" make post request with urllib"""
+if __name__ == "__main__":
+    import urllib.parse
+    import urllib.request
+    from sys import argv
+    url = argv[1]
+    values = {'email' : argv[2]}
+    data = urllib.parse.urlencode(values)
+    print(data)
+    data = data.encode('utf-8')
+    print(data)
+    req = urllib.request.Request(url, data)
+    with urllib.request.urlopen(req) as response:
+        the_page = response.read()
+        print(the_page.decode('utf-8'))
