6941d5fd04bbc2aa567a2c41a929fc5e9ce46117
Jose Medina <fermed28@gmail.com>
2019-08-26 23:56:10 +0000

    [add] basic auth with requests

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -1,1 +1,11 @@
 #!/usr/bin/python3
+""" get id form github """
+if __name__ == "__main__":
+    import requests
+    from sys import argv
+
+    res = requests.get('https://api.github.com/user', auth=(argv[1], argv[2]))
+    if res.json() and res.status_code == 200:
+        print(res.json()['id'])
+    else:
+        print("None")
