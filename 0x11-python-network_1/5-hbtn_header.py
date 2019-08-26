d9bd6c0666cf09386fa8f2960008c77e42010e49
Jose Medina <fermed28@gmail.com>
2019-08-26 21:58:34 +0000

    [add] get header form requests

--- 0x11-python-network_1/5-hbtn_header.py
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -1,1 +1,8 @@
 #!/usr/bin/python3
+""" fetch to specific url"""
+
+if __name__ == "__main__":
+    import requests
+    from sys import argv
+    data = requests.get(argv[1])
+    print(data.headers['X-Request-Id'])
