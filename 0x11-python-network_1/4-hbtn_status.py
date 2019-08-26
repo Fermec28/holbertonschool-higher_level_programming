5fa89e58ed365f2c88c99dde82045e6066af4c66
Jose Medina <fermed28@gmail.com>
2019-08-26 21:55:27 +0000

    [add] get request with requests library

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -1,1 +1,9 @@
 #!/usr/bin/python3
+""" fetch to specific url"""
+
+if __name__ == "__main__":
+    import requests
+    data = requests.get('https://intranet.hbtn.io/status')
+    print("Body response:")
+    print("\t- type: {}".format(type(data.text)))
+    print("\t- content: {}".format(data.text))
