c2a5ef8a9e1b00570b0de44a546c00806e7f15a1
Jose Medina <fermed28@gmail.com>
2019-08-26 23:26:49 +0000

    [add] request to api

--- 0x11-python-network_1/9-starwars.py
+++ 0x11-python-network_1/9-starwars.py
@@ -1,1 +1,12 @@
 #!/usr/bin/python3
+""" request to star wars api"""
+if __name__ == "__main__":
+    import requests
+    from sys import argv
+    URL = "https://swapi.co/api/people"
+    PARAMS = {'search': argv[1]}
+    r = requests.get(url=URL, params=PARAMS)
+    data = r.json()
+    print("Number of results: {}".format(data['count']))
+    for result in data['results']:
+        print(result['name'])
