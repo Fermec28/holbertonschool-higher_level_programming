ea9ffe12b329f1317d9ff4b115f2fca999636215
Jose Medina <fermed28@gmail.com>
2019-08-27 01:17:14 +0000

    [add] get movie from character

--- 0x11-python-network_1/102-starwars.py
+++ 0x11-python-network_1/102-starwars.py
@@ -1,1 +1,30 @@
 #!/usr/bin/python3
+""" request to star wars api"""
+
+
+def get_movie(URL=""):
+    """ function to get title of movie """
+    r = requests.get(url=URL)
+    data = r.json()
+    print("\t{}".format(data['title']))
+
+
+def handler_request(URL="", PARAMS=""):
+    """ function call request"""
+    r = requests.get(url=URL, params=PARAMS)
+    data = r.json()
+    if PARAMS:
+        print("Number of results: {}".format(data['count']))
+    for result in data['results']:
+        print(result['name'])
+        for movie in result['films']:
+            get_movie(movie)
+    if data["next"]:
+        handler_request(data["next"])
+
+if __name__ == "__main__":
+    import requests
+    from sys import argv
+    URL = "https://swapi.co/api/people"
+    PARAMS = {'search': argv[1]}
+    handler_request(URL, PARAMS)
