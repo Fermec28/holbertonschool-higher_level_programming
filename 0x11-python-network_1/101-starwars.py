b89cfe558f374b0c295a6bf037d775437b108d30
Jose Medina <fermed28@gmail.com>
2019-08-27 01:04:43 +0000

    [add] request with pagination

--- 0x11-python-network_1/101-starwars.py
+++ 0x11-python-network_1/101-starwars.py
@@ -1,1 +1,21 @@
 #!/usr/bin/python3
+""" request to star wars api"""
+
+
+def handler_request(URL="", PARAMS=""):
+    """ function call request"""
+    r = requests.get(url=URL, params=PARAMS)
+    data = r.json()
+    if PARAMS:
+        print("Number of results: {}".format(data['count']))
+    for result in data['results']:
+        print(result['name'])
+    if data["next"]:
+        handler_request(data["next"])
+
+if __name__ == "__main__":
+    import requests
+    from sys import argv
+    URL = "https://swapi.co/api/people"
+    PARAMS = {'search': argv[1]}
+    handler_request(URL, PARAMS)
