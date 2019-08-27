d61ee146575d2b520864cfdcf31b7f71e299259d
Jose Medina <fermed28@gmail.com>
2019-08-27 00:28:47 +0000

    [add] commits from github

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -1,1 +1,14 @@
 #!/usr/bin/python3
+""" get id form github """
+if __name__ == "__main__":
+    import requests
+    from sys import argv
+    req = 'https://api.github.com/repos/{}/{}/commits'.format(argv[1], argv[2])
+    print(req)
+    res = requests.get(req)
+    i = 1
+    for commit in res.json():
+        print("{}: {}".format(commit["sha"], commit["commit"]["author"]["name"]))
+        if i == 10:
+            break
+        i += 1
