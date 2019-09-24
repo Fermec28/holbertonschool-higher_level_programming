58455a02568dca33cdc4a69b63163cf0cc19dc88
Jose Medina <fermed28@gmail.com>
2019-09-24 00:30:59 +0000

    [add] create new object

---
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+const dict = require('./101-data').dict;
+const newDict = {};
+for (const [key, value] of Object.entries(dict)) {
+  if (newDict[value]) {
+    newDict[value].push(key);
+  } else {
+    newDict[value] = [key];
+  }
+}
+console.log(newDict);
--- 0x13-javascript_objects_scopes_closures/9-logme.js
+++ 0x13-javascript_objects_scopes_closures/9-logme.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
 let times = 0;
 exports.logMe = function (item) {
-  console.log(`${times++} ${item}`);
+  console.log(`${times++}: ${item}`);
 };
