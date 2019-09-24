149e825ee3d8351bcd23f9bdf0ee2a8317107b86
Jose Medina <fermed28@gmail.com>
2019-09-24 00:17:13 +0000

    [fixes] set correctly the closure

--- 0x13-javascript_objects_scopes_closures/9-logme.js
+++ 0x13-javascript_objects_scopes_closures/9-logme.js
@@ -1,10 +1,5 @@
 #!/usr/bin/node
-
-function constructor () {
-  let times = 0;
-  return function (item) {
-    console.log(`${times++} ${item}`);
-  };
-}
-
-exports.logMe = constructor();
+let times = 0;
+exports.logMe = function (item) {
+  console.log(`${times++} ${item}`);
+};
