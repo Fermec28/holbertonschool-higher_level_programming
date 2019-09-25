3caba03b580c0206881995847fc6d0768679acdc
Jose Medina <fermed28@gmail.com>
2019-09-25 02:28:46 +0000

    [fixes] return in error

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -2,7 +2,7 @@
 const fs = require('fs');
 fs.readFile(process.argv[2], 'utf-8', function (err, data) {
   if (err) {
-    console.log(err);
+    return console.log(err);
   }
   console.log(data);
 });
